64adf829c1023723c904c3dabd2e4eb438ae4335
dhanavishnu13 <vishnukvka@gmail.com>
2023-11-08 18:11:00 +0530

    added registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :session, only: :create
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   resources :expenses
   root "expenses#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
