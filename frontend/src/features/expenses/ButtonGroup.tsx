171939b5dbd7ed18a740b99e8c3c3489396211e5
dhanavishnu13 <vishnukvka@gmail.com>
2023-11-13 16:05:30 +0530

    added confirmation for deletion

--- frontend/src/features/expenses/ButtonGroup.tsx
+++ frontend/src/features/expenses/ButtonGroup.tsx
@@ -2,12 +2,15 @@ import {destoryExpenseAsync} from './expenseSlice'
 
 function ButtonGroup(props:any) {
     function handleClick(e:any){
+        const isConfirmed = window.confirm('Are you sure you want to delete?');
+        if (isConfirmed) {
         const payload={
             expense: {
                 expense_id: props.expense_id
             }
         }
         props.dispatch(destoryExpenseAsync(payload))
+    }
     }
   return (
     <div className='btn-group float'>
